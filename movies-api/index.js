905ebb6d8698604bf5a491784a524129831fe6b0
Constantin Suttner <ogtimes@web.de>
2021-12-02 17:40:11 +0000

    Lab 2Exercise

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
+import genresRouter from './api/genres';
 
 dotenv.config();
 
@@ -10,7 +11,9 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
+app.use('/api/genres', genresRouter);
 app.use('/api/movies', moviesRouter);
+
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
