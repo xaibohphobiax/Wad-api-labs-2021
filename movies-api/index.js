e9f4ffae4fccfc1f5fa79585af323b163f0c651d
Constantin Suttner <ogtimes@web.de>
2021-12-02 19:41:51 +0000

    Mongo lab error handling

--- movies-api/index.js
+++ movies-api/index.js
@@ -8,6 +8,16 @@ import usersRouter from './api/users';
 
 dotenv.config();
 
+const errHandler = (err, req, res, next) => {
+  /* if the error in development then send stack trace to display whole error,
+  if it's in production then just send error message  */
+  // eslint-disable-next-line no-undef
+  if(process.env.NODE_ENV === 'production') {
+    return res.status(500).send(`Something went wrong!`);
+  }
+  res.status(500).send(`Hey!! You caught the error 👍👍. Here's the details: ${err.stack} `);
+};
+
 const app = express();
 
 // eslint-disable-next-line no-undef
@@ -17,6 +27,7 @@ app.use(express.json());
 app.use('/api/movies', moviesRouter);
 app.use('/api/genres', genresRouter);
 app.use('/api/users', usersRouter);
+app.use(errHandler);
 
 
 app.listen(port, () => {
