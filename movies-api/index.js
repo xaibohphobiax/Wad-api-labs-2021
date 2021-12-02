09ad885f94f3acdfb6036e1173b1814a0cb2960a
Constantin Suttner <ogtimes@web.de>
2021-12-02 16:19:34 +0000

    Routes for get movie nad movie details

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,13 +1,15 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import moviesRouter from './api/movies';
 
 dotenv.config();
 
 const app = express();
 
+// eslint-disable-next-line no-undef
 const port = process.env.PORT;
 
-app.use(express.static('public'));
+app.use('/api/movies', moviesRouter);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
