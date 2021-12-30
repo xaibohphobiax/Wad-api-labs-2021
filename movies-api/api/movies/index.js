8aaa467492cf45b1eec74e47babe2fcb70c4512e
Constantin Suttner <ogtimes@web.de>
2021-12-30 16:52:11 +0100

    Added two new API routes trending and popular movies and integrated them.

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -4,6 +4,8 @@ import uniqid from 'uniqid';
 import movieModel from './movieModel';
 import asyncHandler from 'express-async-handler';
 import {getUpcomingMovies} from '../tmdb-api';
+import {getTrendingMovies} from '../tmdb-api';
+import {getPopularMovies} from '../tmdb-api';
 
 const router = express.Router();
 router.get('/', asyncHandler(async (req, res) => {
@@ -70,4 +72,16 @@ router.post('/:id/reviews', (req, res) => {
 router.get('/tmdb/upcoming', asyncHandler( async(req, res) => {
     const upcomingMovies = await getUpcomingMovies();
     res.status(200).json(upcomingMovies);
-  }));
+}));
+
+
+router.get('/tmdb/trending', asyncHandler( async(req, res) => {
+    const trendingMovies = await getTrendingMovies();
+    res.status(200).json(trendingMovies);
+}));
+
+
+router.get('/tmdb/popular', asyncHandler( async(req, res) => {
+    const popularMovies = await getPopularMovies();
+    res.status(200).json(popularMovies);
+}));
--- movies-api/api/tmdb-api.js
+++ movies-api/api/tmdb-api.js
@@ -5,13 +5,46 @@ export const getUpcomingMovies = () => {
         // eslint-disable-next-line no-undef
         `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.TMDB_KEY}&language=en-US&page=1`
     ).then((response) => {
+        console.log(response);
         if (!response.ok) {
             throw new Error(response.json().message);
         }
         return response.json();
     })
         .catch((error) => {
-            throw error
+            throw error;
         });
 };
 //Topic 11 - I've completed all my labs!
+
+export const getTrendingMovies = () => {
+    return fetch(
+      // eslint-disable-next-line no-undef
+      `https://api.themoviedb.org/3/trending/movie/week?api_key=${process.env.TMDB_KEY}&language=en-US&page=2`
+    ).then((response) => {
+      if (!response.ok) {
+        console.log(response);
+        throw new Error(response.json().message);
+      }
+      return response.json();
+    })
+    .catch((error) => {
+       throw error;
+    });
+  };
+
+
+  export const getPopularMovies = () => {
+    return fetch(
+      // eslint-disable-next-line no-undef
+      `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.TMDB_KEY}&language=en-US&page=3`
+    ).then((response) => {
+        if (!response.ok) {
+          throw new Error(response.json().message);
+        }
+        return response.json();
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
